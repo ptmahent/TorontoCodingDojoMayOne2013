559035d2a0f1c368d882014dfe56bbf67743ef3a
Tom Alexandrowicz <talexand@thoughtworks.com>
2013-01-30 20:37:47 -0500

    End of Dojo state

--- src/tompac/core.clj
+++ src/tompac/core.clj
@@ -3,18 +3,67 @@
   (:import java.awt.event.KeyEvent)
   (:gen-class))
 
+
 (defn setup []
 	(smooth)
-	(frame-rate 4)
+	(frame-rate 30)
 	(set-state! 
 			:message (atom "START")
 			:sequence (seq->stream (steps))))
 
 (defn draw []
 	(background 0)
+
+  (ellipse 100 100 20 20)
 	(let [sequencegen (state :sequence)
         sequenceval (sequencegen)]
 		(text (str sequenceval @(state :message)) 20 60)))
+
+(defn pacman-move-left [state]
+  (if (= (-> state :pac-man :x) 0) state 
+  (update-in state [:pac-man :x]  dec)) )
+
+(defn can-move? [pac-man board size direction]
+  (when (= 0 
+    (get board 
+      (inc 
+        (+  (* size (:y pac-man))
+            (:x pac-man))))
+    true
+)))
+
+(defn board [state]
+  (-> state :board :map))
+
+(defn pacman-move-right [state]
+  (let [pac-man (:pac-man state)
+        board (board state)
+        size (-> state :board :size)
+    ]
+    (println "can move?" 
+      (can-move? pac-man board size :right))
+
+  (if (= (-> state :pac-man :x) (-> state :board :right))
+    (assoc-in state [:pac-man :x] 0)
+    (update-in state [:pac-man :x] inc))))
+(defn pacman-move-up [state]
+  (if (= (-> state :pac-man :y) 0) state 
+  (update-in state [:pac-man :y]  dec)) )
+(defn pacman-move-down [state]
+  (if (= (-> state :pac-man :y) 0) state 
+  (update-in state [:pac-man :y]  inc)) )
+
+(defn pacman-move [statemap direction]
+  (case direction
+    :left (pacman-move-left statemap)
+    :right (pacman-move-right statemap)
+    :up (pacman-move-up statemap)
+    :down (pacman-move-down statemap))
+
+) 
+   
+
+
 
 (def valid-keys {
   KeyEvent/VK_UP :up
--- test/tompac/core_test.clj
+++ test/tompac/core_test.clj
@@ -1,6 +1,52 @@
 (ns tompac.core-test
-  (:use clojure.test tompac.core))
+  (:use clojure.test tompac.core quil.core quil.helpers.seqs))
 
-(deftest a-test
-  (testing "Write a test!"
-    (is (= 0 1))))
+
+(deftest pacman-move-test
+  (testing "Next pacman moves pacman to the right"
+    (is (= {:pac-man {:x 21 :y 20}}
+    	   (pacman-move {:pac-man {:x 20 :y 20}} :right)))
+	(let [state {:pac-man {:x 0 :y 1} 
+    	   		:board {:map [1 1
+    	   				      0 1]
+    	   			    :size 2
+    	   			    }
+    	   		}]
+
+	(is (= {:pac-man {:x 0 :y 1}}
+    	   (select-keys 
+    	   	(pacman-move state :right)
+    	   [:pac-man])))))
+
+  (testing "Next pacman moves pacman to the right"
+    (is (= {:pac-man {:x 20 :y 20}}
+    	   (pacman-move {:pac-man {:x 19 :y 20}} :right))))
+
+  (testing "Next pacman moves pacman up"
+    (is (= {:pac-man {:x 20 :y 19}}
+    	   (pacman-move {:pac-man {:x 20 :y 20}} :up))))
+
+  (testing "Next pacman moves pacman down"
+    (is (= {:pac-man {:x 20 :y 21}}
+    	   (pacman-move {:pac-man {:x 20 :y 20}} :down))))
+
+  (testing "Pacman stops when it hits the right wall"
+  	(is (= {:pac-man {:x 20 :y 0}}
+    	   (select-keys 
+    	   	(pacman-move {:pac-man {:x 20 :y 0} :board {:right 20 }} :right)
+    	   [:pac-man]))))
+
+  (testing "Pacman stops when it hits the right wall"
+  	(is (= {:pac-man {:x 21 :y 0}}
+    	   (select-keys 
+    	   	(pacman-move {:pac-man {:x 21 :y 0} :board {:right 21 }} :right)
+    	   [:pac-man]))))
+
+  (testing "Pacman should re-appear at the left end when going at the rightmost"
+  	(is (= {:pac-man {:x 0 :y 0}}
+  		(select-keys
+  			(pacman-move {:pac-man {:x 20 :y 0} :board {:right 20}} :right)
+  			[:pac-man]))))
+
+  )
+
